4d27a44bd8f9a56f375c00fbcf5ebb2b89cb860f
abel1494 <118557596+abel1494@users.noreply.github.com>
2026-04-26 16:09:58 +0700

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -24,7 +24,7 @@ export default function RegisterPage() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("/api/register", {
+      const response = await fetch("https://rag-backend-skripsi.vercel.app/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
